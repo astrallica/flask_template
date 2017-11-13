0bdd66ece8d639c93b2201499bf71bda53ee2698
astrallica <pigrabbit86@gmail.com>
2017-11-14 00:07:20 +0900

    add hello api

--- resources/hello_api.py
+++ resources/hello_api.py
@@ -7,11 +7,11 @@ from common.fields import test_fields
 from common.api_errors import Invalid, NotFound
 
 __author__ = "Philgyu, Seong"
-__email__ = "phil@treasureisle.co"
+__email__ = "phil.seong@funlab.kr"
 
 KEY_NAME = "name"
 
 
 class HelloApi(Resource):
     def get(self):
-        return "hello!!!"
+        return "hi!!!"
--- resources/test_api.py
+++ resources/test_api.py
@@ -7,7 +7,7 @@ from common.fields import test_fields
 from common.api_errors import Invalid, NotFound
 
 __author__ = "Philgyu, Seong"
-__email__ = "phil@treasureisle.co"
+__email__ = "phil.seong@funlab.kr"
 
 KEY_NAME = "name"
 
